afbbca552105cfdf886b1a1ab551124503f68d3b
daviddumke <david.dumke@gmail.com>
2018-05-13 19:46:36 -0300

    Estrutura de arquivos definida

---
+++ docs/index.php
@@ -0,0 +1,14 @@
+<?php require_once('includes/Header.php'); ?>
+
+
+<h1>Vitrine de recomendação por produto</h1>
+<div class="row">
+
+	<div class="col-lg-3 col-md-3 col-sm-4">
+		<h2> Você visitou</h2>
+	</div>
+
+	<div class="col-lg-9 col-md-9 col-sm-8">
+		<h2> e talvez se interesse por:</h2>
+	</div>
+</div>
