db97983242425fce38e5cae89d010cded0e12560
daviddumke <david.dumke@gmail.com>
2018-05-13 20:43:49 -0300

    teste

---
+++ docs/assets/js/function.js
@@ -0,0 +1,25 @@
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+
+
+loadJSON('http://roberval.chaordicsystems.com/challenge/challenge.json?callback=90',
+         function(data) { console.log(data); },
+         function(xhr) { console.error(xhr); }
+);
